c5b18b7404b0770898551fbc8483e66f3a8e84d4
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-08 18:36:08 +0300

    Connected to a remote PostgeSQL database

--- Odigo_django/settings.py
+++ Odigo_django/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'Odigo_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT')
     }
 }
 
